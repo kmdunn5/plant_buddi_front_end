098a1ffee3382384cd2f3588f5212d1a2299ee3f
Brittani Savage <brittanisavage@gmail.com>
2021-03-16 03:43:30 -0500

    Updating React initialization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react'
+import plants from '../../plant-buddi-backend/models/Plant.js'
+
+let baseURL = 'http://localhost:3003'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-      this.state = {
-        plants: []
-      }
+    this.state = {
+      plants: plants
     }
-    render() {
-      return (
-        <div className="App">
-          <h1>Plant Buddi</h1>
-        </div>
-  );
-}
-}
+    this.getPlants = this.getPlants.bind(this)
+  }
+  componentDidMount(){
+    this.getPlants()
+  }
+  getPlants = () => {
+    fetch(baseURL + '/plants')
+      .then(data => {
+        return data.json()},
+        err => console.log(err))
+      .then(parsedData => console.log(parsedData),
+       err=> console.log(err))
+  }
+
+  render() {
+    return (
+      <div className='container'>
+       <h1>Plant Buddi</h1>
+         {this.state.plants.map(plant => {
+           return (
+           <div key={plants._id}>  
+              <h3> {plants.commonName} </h3>
+              <h3> {plants.scientificName} </h3>
+            </div>
+           )}
+         )}
+         </div>
+         )}
+        
+
+
 
 export default App;
