d3e0a516ba10f0c335ae3c993b045affc148fd22
Brittani Savage <brittanisavage@gmail.com>
2021-03-18 09:28:22 -0500

    styled header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Plants from './components/Plants.jsx'
-import Home from './components/Home.jsx'
-import SignUp from './components/SignUp.jsx'
-import Login from './components/Login.jsx'
-import Search from './components/Search.jsx'
+// import Home from './components/Home.jsx'
+// import SignUp from './components/SignUp.jsx'
+// import Login from './components/Login.jsx'
+// import Search from './components/Search.jsx'
 
 
 class App extends Component {
@@ -15,8 +15,8 @@ class App extends Component {
     return (
       <div>
         < Header />
-        < Home />
-        {/* < Plants /> */}
+        {/* < Home /> */}
+        < Plants />
         < Footer />
       </div>
     )
