e5f19d7dc5c99c3e7a09798c214197cc20c435c1
kmdunn5 <kmdunn5@gmail.com>
2021-03-18 00:08:03 -0400

    uncommented <plants>

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div>
         < Header />
         {/* < Home /> */}
-        {/* < Plants /> */}
+        < Plants />
         < Footer />
       </div>
     )
